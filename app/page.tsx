81235861f881bf6806ccc871ca7a5e20ef028dac
Tony Popenkov <popenkovaa@yandex.ru>
2024-03-13 13:24:51 +0300

    feat: button added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/src/shared/ui';
 import { AppLink } from '@/src/shared/ui/AppLink/AppLink';
 
 export default function Home() {
   return (
     <>
       <AppLink href={'#'}>gsdfgdfg</AppLink>
+      <Button>321132</Button>
     </>
   );
 }
--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,77 +1,69 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { AppLink, AppLinkTheme, AppLinkSize } from './AppLink';
+import { Button, ButtonTheme, ButtonSize } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'shared/AppLink',
-  component: AppLink,
+  title: 'shared/Button',
+  component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  args: {
-    href: '/',
-  },
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} satisfies Meta<typeof AppLink>;
+} satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-
 export const PrimarySmall: Story = {
   args: {
     children: 'Text',
-    theme: AppLinkTheme.PRIMARY,
-    size: AppLinkSize.SMALL,
+    theme: ButtonTheme.PRIMARY,
+    size: ButtonSize.SMALL,
   },
 };
 
 export const Primary: Story = {
   args: {
     children: 'Text',
-    theme: AppLinkTheme.PRIMARY,
+    theme: ButtonTheme.PRIMARY,
   },
 };
 
 export const PrimaryLarge: Story = {
   args: {
     children: 'Text',
-    theme: AppLinkTheme.PRIMARY,
-    size: AppLinkSize.LARGE,
+    theme: ButtonTheme.PRIMARY,
+    size: ButtonSize.LARGE,
   },
 };
 
 export const Ghost: Story = {
   args: {
     children: 'Text',
-    theme: AppLinkTheme.GHOST,
+    theme: ButtonTheme.GHOST,
   },
 };
 
 export const White: Story = {
   args: {
     children: 'Text',
-    theme: AppLinkTheme.WHITE,
+    theme: ButtonTheme.WHITE,
   },
 };
 
 export const Border: Story = {
   args: {
     children: 'Text',
-    theme: AppLinkTheme.BORDER,
+    theme: ButtonTheme.BORDER,
   },
 };
 
 export const Gray: Story = {
   args: {
     children: 'Text',
-    theme: AppLinkTheme.GRAY,
+    theme: ButtonTheme.GRAY,
   },
 };
