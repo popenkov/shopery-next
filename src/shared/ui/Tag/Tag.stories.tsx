a1ec0a002268706ee71a6f0b19ba155fbd55440e
Tony Popenkov <popenkovaa@yandex.ru>
2024-03-14 16:31:19 +0300

    feat: countdown hook, mock data and api route added

--- src/shared/ui/Tag/Tag.stories.tsx
+++ src/shared/ui/Tag/Tag.stories.tsx
@@ -1,26 +1,42 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Logo, LogoSize } from './Tag';
+import { Tag, TagVariant } from './Tag';
 
 const meta = {
-  title: 'shared/Logo',
-  component: Logo,
+  title: 'shared/Tag',
+  component: Tag,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
-} satisfies Meta<typeof Logo>;
+} satisfies Meta<typeof Tag>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const DefaultLogo: Story = {
+export const RedTag: Story = {
   args: {
-    size: LogoSize.DEFAULT,
+    content: 'Text',
+    variant: TagVariant.RED,
   },
 };
 
-export const SmallLogo: Story = {
+export const BlueTag: Story = {
   args: {
-    size: LogoSize.SMALL,
+    content: 'Text',
+    variant: TagVariant.BLUE,
+  },
+};
+
+export const OrangeTag: Story = {
+  args: {
+    content: 'Text',
+    variant: TagVariant.ORANGE,
+  },
+};
+
+export const BlackTag: Story = {
+  args: {
+    content: 'Text',
+    variant: TagVariant.BLACK,
   },
 };
