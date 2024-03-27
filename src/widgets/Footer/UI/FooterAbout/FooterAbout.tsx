ed6cf50c53c3878799050dfc569293e58fab9f04
Tony Popenkov <popenkovaa@yandex.ru>
2024-03-27 22:18:29 +0300

    fix(WIP): refactoring in progress

--- src/widgets/Footer/UI/FooterAbout/FooterAbout.tsx
+++ src/widgets/Footer/UI/FooterAbout/FooterAbout.tsx
@@ -1,5 +1,7 @@
 import React, { DetailedHTMLProps, FC, HTMLAttributes } from 'react';
 
+import { Text } from '@/shared';
+
 import cls from './FooterAbout.module.scss';
 
 interface Props
@@ -8,16 +10,21 @@ interface Props
 export const FooterAbout: FC<Props> = () => {
   return (
     <div className={cls.about}>
-      <h2 className={cls.aboutTitle}>About Shopery</h2>
-      <p className={cls.aboutText}>
+      <Text variant="body_xl" as="h2" className={cls.aboutTitle}>
+        About Shopery
+      </Text>
+      <Text variant="body_s" className={cls.aboutText}>
         Morbi cursus porttitor enim lobortis molestie. Duis gravida turpis dui,
         eget bibendum magna congue nec.
-      </p>
+      </Text>
+
       <div className={cls.aboutContact}>
         <a className={cls.aboutLink} href="tel:(219)555-0114">
           (219) 555-0114
         </a>
-        <span className={cls.separator}>or</span>
+        <Text variant="body_s" as="span" className={cls.separator}>
+          or
+        </Text>
         <a className={cls.aboutLink} href="mailto:proxy@gmail.com">
           Proxy@gmail.com
         </a>
