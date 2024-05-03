04836875203386e66120ad54f932b3c3c04f38c4
Tony Popenkov <popenkovaa@yandex.ru>
2024-05-03 08:48:59 +0300

    feat(WIP): api added

---
+++ api/backend/app/controllers/index.js
@@ -0,0 +1,2 @@
+export * as ProductsController from './ProductsController.js';
+export * as UserController from './UserController.js';
---
+++ api/backend/app/models/index.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import { dbConfig } from '../config/db.config.js';
+
+// const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const db = {};
+db.mongoose = mongoose;
+db.url = dbConfig.url;
+// todo
+// db.products = require('./tutorial.model.js')(mongoose);
+
+export { db };
