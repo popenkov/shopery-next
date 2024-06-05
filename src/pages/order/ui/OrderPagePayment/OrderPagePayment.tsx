536c59c04085c3a8772b8c8733f72b59a2aa33b1
Tony Popenkov <popenkovaa@yandex.ru>
2024-06-05 17:42:53 +0300

    feat(WIP): work on order detailed page

--- src/pages/order/ui/OrderPagePayment/OrderPagePayment.tsx
+++ src/pages/order/ui/OrderPagePayment/OrderPagePayment.tsx
@@ -1,7 +1,76 @@
 import { FC } from 'react';
+import cn from 'classnames';
+
+import { Text } from '@/shared/ui/Text';
 
 import cls from './OrderPagePayment.module.scss';
 
-export const OrderPagePayment: FC = () => {
-  return <div className={cls.OrderPagePayment}>OrderPagePayment</div>;
+type Props = {
+  className?: string;
+};
+
+export const OrderPagePayment: FC<Props> = ({ className }) => {
+  return (
+    <div className={cn(cls.OrderPagePayment, className)}>
+      <div className={cls.OrderPagePaymentHeader}>
+        <div className={cls.OrderPagePaymentColumn}>
+          <Text variant="body_s" className={cls.OrderPagePaymentColumnKey} as="span">
+            Order ID:
+          </Text>
+          <span className={cls.OrderPagePaymentHeaderSeparator} />
+          <Text variant="body_s" className={cls.OrderPagePaymentColumnValue} as="span">
+            #4152
+          </Text>
+        </div>
+        <div className={cls.OrderPagePaymentColumn}>
+          <Text variant="body_s" className={cls.OrderPagePaymentKey} as="span">
+            Payment Method:
+          </Text>
+          <Text variant="body_s" weight="medium" className={cls.OrderPagePaymentValue} as="span">
+            Paypal
+          </Text>
+        </div>
+      </div>
+      <div className={cls.OrderPagePaymentValues}>
+        <div className={cls.OrderPagePaymentRow}>
+          <Text variant="body_s" className={cls.OrderPagePaymentKey} as="span">
+            Subtotal:
+          </Text>
+          <Text variant="body_s" weight="medium" className={cls.OrderPagePaymentValue} as="span">
+            $365.00
+          </Text>
+        </div>
+        <div className={cls.OrderPagePaymentRow}>
+          <Text variant="body_s" className={cls.OrderPagePaymentKey} as="span">
+            Discount:
+          </Text>
+          <Text variant="body_s" weight="medium" className={cls.OrderPagePaymentValue} as="span">
+            20%
+          </Text>
+        </div>
+        <div className={cls.OrderPagePaymentRow}>
+          <Text variant="body_s" className={cls.OrderPagePaymentKey} as="span">
+            Shipping:
+          </Text>
+          <Text variant="body_s" weight="medium" className={cls.OrderPagePaymentValue} as="span">
+            Free
+          </Text>
+        </div>
+
+        <div className={cls.OrderPagePaymentRow}>
+          <Text variant="body_l" className={cls.OrderPagePaymentKeyBold} as="span">
+            Total:
+          </Text>
+          <Text
+            variant="body_l"
+            weight="semibold"
+            className={cls.OrderPagePaymentTotalPrice}
+            as="span"
+          >
+            $84.00
+          </Text>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/pages/order/ui/OrderPageStatus/OrderPageStatus.tsx
+++ src/pages/order/ui/OrderPageStatus/OrderPageStatus.tsx
@@ -1,12 +1,58 @@
-import { FC, memo } from 'react';
+import { FC, memo, useEffect, useState } from 'react';
+import cn from 'classnames';
+
+import { Text } from '@/shared/ui/Text';
 
 import cls from './OrderPageStatus.module.scss';
+
 type Props = {
-  status: 'Processing' | 'On the way' | 'Order Received' | 'Delivered';
+  status: 'Order Received' | 'Processing' | 'On the way' | 'Delivered';
+  className?: string;
 };
 
-export const OrderPageStatus: FC<Props> = memo(({ status }) => {
-  return <div className={cls.OrderPageStatus}>{status}</div>;
+export const OrderPageStatus: FC<Props> = memo(({ status, className }) => {
+  const [percent, setPercent] = useState(0);
+
+  useEffect(() => {
+    switch (status) {
+      case 'Order Received':
+        setPercent(15);
+        break;
+      case 'Processing':
+        setPercent(45);
+        break;
+      case 'On the way':
+        setPercent(75);
+        break;
+      case 'Delivered':
+        setPercent(100);
+        break;
+      default:
+        setPercent(0);
+    }
+  }, [status]);
+
+  return (
+    <div className={cn(cls.OrderPageStatus, className)}>
+      <div className={cls.OrderPageStatusBar}>
+        <div className={cls.OrderPageStatusBarActive} style={{ width: `${percent}%` }}></div>
+      </div>
+      <div className={cls.OrderPageStatusContent}>
+        <div className={cls.OrderPageStatus}>
+          <Text>Order received</Text>
+        </div>
+        <div className={cls.OrderPageStatus}>
+          <Text>Processing</Text>
+        </div>
+        <div className={cls.OrderPageStatus}>
+          <Text>On the way</Text>
+        </div>
+        <div className={cls.OrderPageStatus}>
+          <Text>Delivered</Text>
+        </div>
+      </div>
+    </div>
+  );
 });
 
 OrderPageStatus.displayName = 'OrderPageStatus';
